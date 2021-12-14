3db7d2136fe02635728b6b6fd7e75e17d8d6e099
michaellosdev <mishaloskutov@gmail.com>
2021-12-14 00:32:14 -0800

    fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ import tiktok from '../tiktok.png'
 export default function footer()  {
     return (
         <div className="footer--container">
-            <img src={github} alt='github icon' className="icon"/>
-            <img src={insta} alt='instagram icon' className="icon"/>
-            <img src={tiktok} alt='tiktok icon' className="icon"/>
+            <a href="https://github.com/michaellosdev"><img src={github} alt='github icon' className="icon"/></a>
+            <a href="https://www.instagram.com/michaellos/"><img src={insta} alt='instagram icon' className="icon"/></a>
+            <a href="https://www.tiktok.com/@mishatherussiann?lang=en"><img src={tiktok} alt='tiktok icon' className="icon"/></a>
         </div>
     )
 }
